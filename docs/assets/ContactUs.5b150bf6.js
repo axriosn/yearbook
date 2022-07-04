fe021f500fa93e95c662d240fa1e0cea29061224
axriosn <axriosn@gmail.com>
2022-07-04 12:43:37 -0400

    change vite base for github-pages and build

---
+++ docs/assets/ContactUs.5b150bf6.js
@@ -0,0 +1,2 @@
+import{_ as e,c as t,o as a}from"./index.d795c4fe.js";const o={name:"ContactUs"},r={src:"https://docs.google.com/forms/d/e/1FAIpQLSddZmfwjdLUALHAS4HTRA1yDXFeVOoIwH0D-OPneRAMPbGxyQ/viewform?embedded=true",width:"640",height:"947",class:"iframe"};function s(c,d,n,_,i,m){return a(),t("iframe",r,`Loading\u2026\r
+  `)}var f=e(o,[["render",s],["__scopeId","data-v-287da3ce"]]);export{f as default};
